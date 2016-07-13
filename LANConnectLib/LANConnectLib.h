53e8b509f3fdfe06e522fee3a09fc9fdb63863d9
linjiejiao <hpckljj@gmail.com>
2016-07-13 15:11:58 +0800

    first commit

---
+++ LANConnectLib/LANConnectLib.h
@@ -0,0 +1,19 @@
+//
+//  LANConnectLib.h
+//  LANConnectLib
+//
+//  Created by liangjiajian_mac on 16/7/13.
+//  Copyright © 2016年 cn.ljj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LANConnectLib.
+FOUNDATION_EXPORT double LANConnectLibVersionNumber;
+
+//! Project version string for LANConnectLib.
+FOUNDATION_EXPORT const unsigned char LANConnectLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LANConnectLib/PublicHeader.h>
+
+
